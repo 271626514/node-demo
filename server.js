65367d94de0392e55dc0a8e0a2f8148ad35bd953
qiankun <syzx9801@163.com>
2017-12-31 00:08:45 +0800

    update

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 /**
  * Created by syzx9801@163.com on 2017/12/29.
  */
-const http = require("http"),
-  io = require("socket.io")
+const koa = require('koa'),
+  server = require('http').Server(koa),
+  io = require("socket.io")(server),
+  app = new koa(),
+  router = require('koa-route'),
+  route = require('./route');
 
-const server = http.createServer((req, res) => {
-  res.end()
-}).listen(9919)
+app.use(router.get('/', route.lottery));
+app.use(router.get('/vote', route.vote));
 
-const socket = io.listen(server)
+app.listen(9918, '192.168.1.105');
+server.listen(9919, '192.168.1.105');
 
-socket.of('/console').on('connection', client => {
+io.of('/init').on('connection', client => {
   console.log(`连接成功`)
   let msg = {
     'code': 200,
@@ -19,8 +23,9 @@ socket.of('/console').on('connection', client => {
   client.emit('data', msg)
 })
 
-socket.on("connection", client => {
+io.of('/count').on("connection", client => {
   client.on('acount', acount => {
+    console.log(acount)
     client.emit('addCount', acount.id)
   })
 })
