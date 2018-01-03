4a64a977997e6b50977b1ce09ffa8f20f24d7cef
qiankun <syzx9801@163.com>
2018-01-03 23:45:36 +0800

    Default

--- router/api.js
+++ router/api.js
@@ -2,15 +2,34 @@
  * Created by syzx9801@163.com on 2018/1/2.
  */
 const Router = require('koa-router');
+const koaBody = require('koa-body');
 
-const router = new Router({
+const api = new Router({
   prefix: '/api'
 })
   .get('/getTime', async ctx => {
     ctx.response.type = 'json';
-    ctx.response.body = {"code":"200","msg":new Date().getTime()};
+    ctx.response.body = {"code":"200", "data":{"localTime": new Date().getTime()}};
+  })
+  .post('/login', koaBody(), async ctx => {
+    console.log(ctx.request.body);
+    let body = ctx.request.body;
+    let data;
+    try {
+      data = {
+        "code": "200",
+        "data": {
+          "name": body.name,
+          "pwd": body.pwd
+        }
+      }
+    } catch (e) {
+
+    }
+  //  ctx.body = `login success! Hello,${ctx.name}. And your pwd is ${ctx}!`
+    ctx.body = data;
   })
 
 module.exports = {
-  router
+  api
 }
--- router/route.js
+++ router/route.js
@@ -3,17 +3,22 @@
  */
 const fs = require('fs');
 const Router = require('koa-router');
+
 const router = new Router()
-  .get('/vote', async ctx => {
-    ctx.response.type = 'html';
-    ctx.response.body = fs.createReadStream('./src/vote.html');
-  })
   .get('/', async ctx => {
     ctx.response.type = 'html';
     ctx.response.body = fs.createReadStream('./src/tpl/index.ejs');
   })
+  .get('/vote', async ctx => {
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream('./src/tpl/vote.ejs');
+  })
   .get('/test', async ctx => {
     ctx.response.body = 'Hello World';
+  })
+  .get('/form', async ctx => {
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream('./src/tpl/form.ejs');
   })
 
 module.exports = {
